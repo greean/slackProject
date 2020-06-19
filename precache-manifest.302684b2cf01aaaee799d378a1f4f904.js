617fb5d560734dce3a515f47b35ecc3c34cdd097
greean <aj.green07@outlook.com>
2020-06-19 08:26:05 +0100

    Updates

---
+++ precache-manifest.302684b2cf01aaaee799d378a1f4f904.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "525d24dd73de9e85145f52c94164734d",
+    "url": "/slackProject/index.html"
+  },
+  {
+    "revision": "5f249e57a3e4f6ad2a12",
+    "url": "/slackProject/static/css/main.7bb2e3dc.chunk.css"
+  },
+  {
+    "revision": "603be6601fd7f0a1393b",
+    "url": "/slackProject/static/js/2.e63c1e99.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/slackProject/static/js/2.e63c1e99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f249e57a3e4f6ad2a12",
+    "url": "/slackProject/static/js/main.29e226aa.chunk.js"
+  },
+  {
+    "revision": "a678809fb8f597a431f8",
+    "url": "/slackProject/static/js/runtime-main.e7e42282.js"
+  },
+  {
+    "revision": "9e31161108ad69cc75264c71274e8a58",
+    "url": "/slackProject/static/media/CircularStd-Book.9e311611.ttf"
+  },
+  {
+    "revision": "e4ba63a6345c96f3cb813ef00340beda",
+    "url": "/slackProject/static/media/CircularStd-Medium.e4ba63a6.ttf"
+  },
+  {
+    "revision": "4fa8e1afb9bbf3d2f3a20c166b6c8132",
+    "url": "/slackProject/static/media/textbox_left.4fa8e1af.png"
+  },
+  {
+    "revision": "f22b826a29f61be778676dfb66cfbd47",
+    "url": "/slackProject/static/media/textbox_right.f22b826a.png"
+  },
+  {
+    "revision": "d7ed1c40763095040741a7d51fffc33d",
+    "url": "/slackProject/static/media/user1.d7ed1c40.png"
+  },
+  {
+    "revision": "87db3ab4fef40c5b6d4d6a64e8ae18c6",
+    "url": "/slackProject/static/media/user2.87db3ab4.png"
+  },
+  {
+    "revision": "821df8bf8a7d3466dfa7771f6ffc0bb6",
+    "url": "/slackProject/static/media/user3.821df8bf.png"
+  }
+]);
